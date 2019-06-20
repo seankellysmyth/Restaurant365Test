8bd30e9bc3c4e7f59a63b235fc1ef4d689365476
Sean Smyth <seankellysmyth@gmail.com>
2019-06-20 11:21:09 -0700

    Adding unit tests

--- Restaurant365Test.Test/UnitTest1.cs
+++ Restaurant365Test.Test/UnitTest1.cs
@@ -1,20 +1,46 @@
 using NUnit.Framework;
-//using Restaurant365Test;
+using Restaurant365Test;
 
 namespace Tests
 {
     public class Tests
     {
+        private Calculator _calculator;
+
         [SetUp]
         public void Setup()
         {
+            _calculator = new Calculator();
         }
 
         [Test]
-        public void Add()
+        public void NoDelimiter()
         {
-            //Calculator.Add("");
-            Assert.Pass();
+            Assert.AreEqual(_calculator.Add("1\n2,3"), 6);
+        }
+
+        [Test]
+        public void SingleOneCharDelim()
+        {
+            Assert.AreEqual(_calculator.Add("//[;]\n1;2;3;4;10000"), 10);
+        }
+
+        [Test]
+        public void MultOneCharDelim()
+        {
+            Assert.AreEqual(_calculator.Add("//[;][*]\n1;2**3**4**10000"), 10);
+        }
+
+        [Test]
+        public void SingleMultCharDelim()
+        {
+            Assert.AreEqual(_calculator.Add("//[**]\n1**2**3**4**10000"), 10);
+        }
+
+        [Test]
+        public void MultMultCharDelim()
+        {
+            Assert.AreEqual(_calculator.Add("//[;;][**]\n1;;2**3**4**10000"), 10);
         }
     }
 }
